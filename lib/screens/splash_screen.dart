277926618a5ba6809118d238347fef17faed7393
riyas-dcs <108170415+datacube-softwares@users.noreply.github.com>
2025-09-09 17:27:14 +0530

    - Added a splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -27,16 +27,25 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        // Match background color to theme
-        color: Theme.of(context).colorScheme.surface,
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFA9BEC3), // a9bec3
+              Color(0xFFE2F5F3), // e2f5f3
+              Color(0xFF84DCC4), // 84dcc4
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         child: Center(
           child: Image.asset(
-            'assets/logo.png',
+            'assets/gr_logo.webp',
             width: 150,
             height: 150,
-            // This ensures the background of the image matches the screen
-            color: Theme.of(context).colorScheme.surface,
-            colorBlendMode: BlendMode.dstIn,
+            fit: BoxFit.cover,
           ),
         ),
       ),
